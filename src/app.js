f84536c379726b68d3b775c8325fe5f1dbff7111
perseus369 <136762632+perseus369@users.noreply.github.com>
2025-01-06 17:47:08 +0000

    Updated Code

--- src/app.js
+++ src/app.js
@@ -16,8 +16,26 @@ let when = [
   "while I was praying"
 ];
 
+function getRandomNumber(a, b) {
+  return Math.floor(Math.random() * (b - a) + a);
+}
+
+function getRandom(anyArray) {
+  let max = anyArray.length - 1;
+  let min = 0;
+  let random = getRandomNumber(min, max);
+  return anyArray[random];
+}
+
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
-  document;
+  document.querySelector("#excuse").innerHTML =
+    getRandom(who) +
+    " " +
+    getRandom(action) +
+    " " +
+    getRandom(what) +
+    " " +
+    getRandom(when);
 };
